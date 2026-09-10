57ed7d7c3c850ea7a3ec2399794717ec558c4ec7
samwel.mwamakula <samwelmwamakula@gmail.com>
2026-09-10 10:47:49 +0300

    [BUILD] - added implenattion of conversation with comma separated .

--- E2CSEPARATED.RESOURCES/Program.cs
+++ E2CSEPARATED.RESOURCES/Program.cs
@@ -1,1 +1,63 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string projectRoot = Path.GetFullPath(Path.Combine(baseDirectory, @"..\..\.."));
+        string excelDir = Path.Combine(projectRoot, "Excel Files");
+        string csvDirectory = Path.Combine(projectRoot, "Csv Files");
+
+        Directory.CreateDirectory(excelDir);
+        Directory.CreateDirectory(csvDirectory);
+
+        Console.WriteLine("Excel folder" + excelDir);
+        Console.WriteLine("Csv folder" + csvDirectory);
+
+        var excelFiles = Directory.GetFiles(excelDir,".xlsx");
+        if(excelFiles.Length == 0)
+        {
+            Console.WriteLine("no excel found in " + excelDir);
+            return;
+        }
+
+        foreach(var excelPath in excelFiles)
+        {
+            try
+            {
+                using var workbook = new XLWorkbook(excelPath);
+                var worksheet = workbook.Worksheet(1);
+                string fileName = Path.GetFileNameWithoutExtension(excelPath);
+                string csvPath = Path.Combine(csvDirectory, fileName + ".csv");
+                using var writer = new StreamWriter(csvPath, false, Encoding.UTF8);
+                foreach(var row in worksheet.RowsUsed())
+                {
+                    var cells = row.CellsUsed();
+                    string line = string.Join(",", cells.Select(c => EscapeCsv(c.GetValue<string>())));
+                    writer.WriteLine(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR:{ex.Message}");
+            }
+        }
+
+
+        static string EscapeCsv(string value)
+        {
+            if (value.Contains(",") || value.Contains("\"") || value.Contains("\n"))
+            {
+                value = "\"" + value.Replace("\"", "\"\"") + "\"";
+            }
+            return value;
+        }
+    }
+
+
+
+}
